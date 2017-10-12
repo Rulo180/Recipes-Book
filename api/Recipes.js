398f1659c1aab8f33ddbb8613e3f9f96453d3bac
Rulo180 <rullencio180@gmail.com>
2017-10-12 17:55:23 -0300

    Step 7 Complete.

---
+++ api/Recipes.js
@@ -0,0 +1,37 @@
+import SimpleSchema from 'simpl-schema';
+SimpleSchema.extendOptions(['autoform']);
+
+Recipes = new Mongo.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+    name: {
+        label: "Name",
+        type: String
+    },
+    desc: {
+        label: "Description",
+        type: String
+    },
+    author: {
+        type: String,
+        label: "Author",
+        autoform: {
+            type: "hidden"
+        },
+        autoValue: function(){
+            return this.Meteor.userId();
+        },
+    },
+    createdAt: {
+        type: Date,
+        label: "CreatedAt",
+        autoform: {
+            type: "hidden"
+        },
+        autoValue: function() {
+            return new Date();
+        },
+    }
+});
+
+Recipes.attachSchema(RecipeSchema);
